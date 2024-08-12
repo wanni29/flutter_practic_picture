8bb7dfa21417d89e3406fe997d0cd07eeaf19901
wanni30 <hubhy@nate.com>
2024-08-12 12:49:49 +0900

    색상 정하기

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColorsV1 {
+  // main Color
+  static const mainCobalt = Color(0xFF29c8d6);
+  static const mainCobaltLight = Color(0xFF00CDE4);
+  static const mainBrown = Color(0xFF382111);
+  static const mainGray = Color(0xFFb6d1de);
+
+  // basic Color
+  static const black = Colors.black;
+  static const white = Colors.white;
+  static const gray = Colors.grey;
+  static const lightGray = Color(0xFFD3D3D3);
+}
